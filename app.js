047cd859c74f9aee50e5d1cb22234a01c67c07ec
kimk1029 <kimk1029@naver.com>
2018-09-21 03:08:46 +0900

    ss

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var express = require('express'); // 웹서버 사용 .
 var app = express();
 var fs = require('fs'); // 파일 로드 사용.
 
-process.env.PWD = process.cwd();
+
 // 포트 설정
 app.listen(3000, function () {
     console.log('Server Start .');
 });
-app.use(express.static(process.env.PWD + '/public'));
+app.use(express.static(__dirname + '/public' ));
 // 라우팅 설정
 app.get('/', function (req, res) { // 웹서버 기본주소로 접속 할 경우 실행 . ( 현재 설정은 localhost 에 3303 port 사용 : 127.0.0.1:3303 )
     fs.readFile('index.html', function (error, data) { // index.html 파일 로드 .
         if (error) {
             console.log(error);
         } else {
-          
+            res.writeHead(200, { 'Content-Type': 'text/html' }); // Head Type 설정 .
             res.end(data); // 로드 html response .
         }
     });
