05881560c754b5c99a7a5f3833921c4d4e278307
Kyuhyun Kim <kyuhyun.kim@mytrum.com>
2018-10-12 17:15:31 +0900

    wd

--- app.js
+++ app.js
@@ -21,3 +21,14 @@ app.get('/', function (req, res) { // 웹서버 기본주소로 접속 할 경�
     });
 });
 
+app.get('/board', function (req, res) { // 웹서버 기본주소로 접속 할 경우 실행 . ( 현재 설정은 localhost 에 3303 port 사용 : 127.0.0.1:3303 )
+    fs.readFile('test_board.html', function (error, data) { // index.html 파일 로드 .
+        if (error) {
+            console.log(error);
+        } else {
+        
+            res.end(data); // 로드 html response .
+        }
+    });
+});
+
